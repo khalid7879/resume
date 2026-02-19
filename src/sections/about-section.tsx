ccee394613198f1d9ff4b267e5ee59a985ab7a7e
khalid7879 <khalidhasantalukder5@gmail.com>
2026-02-19 19:29:33 +0600

    gg

---
+++ src/sections/about-section.tsx
@@ -0,0 +1,15 @@
+import Section from "../components/section";
+
+export default function AboutSection() {
+    return (
+        <Section title="About">
+            <div className="text-sm/6.5">
+                <p>
+                </p>
+                <p className="mt-5">
+                    
+                </p>
+            </div>
+        </Section>
+    );
+}
---
+++ src/sections/contact-section.tsx
@@ -0,0 +1,20 @@
+import Section from "../components/section";
+
+export default function ContactSection() {
+    return (
+        <Section title="Contact">
+            <table className="table-auto mr-auto">
+                <tbody>
+                    <tr>
+                        <td className="pr-4 py-2">Email:</td>
+                        <td className="py-2 text-gray-500">cse.khalid@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td className="pr-4 py-2">Phone:</td>
+                        <td className="py-2 text-gray-500">+880 1710281427</td>
+                    </tr
+                </tbody>
+            </table>
+        </Section>
+    );
+}
